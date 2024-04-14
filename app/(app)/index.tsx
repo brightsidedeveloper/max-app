f5d7bbc0e998999ba07e9dad4649a9ebd318da67
Tim <tim@brightsidedeveloper.com>
2024-04-14 12:06:25 -0500

    Supabase Session Added

---
+++ app/(app)/index.tsx
@@ -0,0 +1,9 @@
+import { SafeAreaView, Text } from 'react-native'
+
+export default function index() {
+  return (
+    <SafeAreaView className='flex-1 items-center justify-center'>
+      <Text className='text-blue-500 text-3xl'>Hello Tim!</Text>
+    </SafeAreaView>
+  )
+}
